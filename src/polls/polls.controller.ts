83e71261271ad02601b908052f2fca7ffa96b5d0
ilkayus <ilkay.devecioglu@gmail.com>
2023-03-20 08:36:33 +0300

    swagger test - pollscontroller&dtos

--- src/polls/polls.controller.ts
+++ src/polls/polls.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { CreatePollDto } from './dto/create-poll.dto';
+import { JoinPollDto } from './dto/join-poll.dto';
 
 @Controller('polls')
 export class PollsController {
+  // TODO - add constructor for access to providers!
+
   @Post()
-  async create() {
-    Logger.log('In Create');
+  async create(@Body() createPollDto: CreatePollDto) {
+    Logger.log('In create!');
+
+    return createPollDto;
   }
 
   @Post('/join')
-  async join() {
-    Logger.log('In Join');
+  async join(@Body() joinPollDto: JoinPollDto) {
+    Logger.log('In join!');
+
+    return joinPollDto;
   }
 
   @Post('/rejoin')
   async rejoin() {
-    Logger.log('In Rejoin');
+    Logger.log('In rejoin!');
+    // TODO - add implementation for extracting user from token
+
+    return {
+      message: 'rejoin endpoint',
+    };
   }
 }
