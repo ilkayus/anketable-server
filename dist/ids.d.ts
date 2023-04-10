71503e97c50ec1f924ad1a1bdeec22565239d4d5
ilkayus <ilkay.devecioglu@gmail.com>
2023-04-10 14:06:17 +0300

    cicd

--- dist/ids.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const createPollID: (size?: number) => string;
-export declare const createUserID: () => string;
-export declare const createNominationID: () => string;
--- dist/polls/helpers/getPollResults.d.ts
+++
@@ -1,3 +0,0 @@
-import { Nominations, Rankings, Results } from '../types/polls.types';
-declare const _default: (rankings: Rankings, nominations: Nominations, votesPerVoter: number) => Results;
-export default _default;
